82116b1f425e457f5aacd7556039fc307a86214a
ankit <sahuankit.murhu@gmail.com>
2025-01-03 19:55:46 +0530

    Delete button added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import "./global.css";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Delete", "Are you sure you want to delete?", [
+      {
+        text: "yes",
+        onPress: () => console.log("Ok deleting"),
+        style: "destructive",
+      },
+      {
+        text: "no",
+        style: "cancel",
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View className="border-red-500 border-b-2 p-2 px-5 w-screen flex-row justify-between items-center">
+        <Text className="text-lg">Shopping list</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          activeOpacity={0.8}
+          className="bg-zinc-900 p-2 rounded-md"
+        >
+          <Text className="text-zinc-200 font-bold uppercase h-auto tracking-wider">
+            Delete
+          </Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
