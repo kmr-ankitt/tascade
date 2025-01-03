90fd28985509a83dbc4f7ca4f38398658618a386
ankit <sahuankit.murhu@gmail.com>
2025-01-03 23:02:28 +0530

    Tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,36 @@
-import { Stack } from "expo-router";
+import { AntDesign, Feather } from "@expo/vector-icons";
+import { Stack, Tabs } from "expo-router";
 
 export default function layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="clock" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="bulb1" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/counter.tsx
+++ app/counter.tsx
@@ -1,17 +1,8 @@
-import { useRouter } from "expo-router";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function CounterScreen() {
-  const router = useRouter();
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          router.push("/idea");
-        }}
-      >
-        <Text>Go to idea</Text>
-      </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
     </View>
   );
