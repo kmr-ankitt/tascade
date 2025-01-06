3177d7b83f237796c45725e1d263a0c19e0c20c3
ankit <sahuankit.murhu@gmail.com>
2025-01-06 08:47:40 +0530

    Notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,36 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import * as Notifications from "expo-notifications";
+import registerForPushNotification from "../../utils/registerForPushNotification";
 
 export default function CounterScreen() {
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotification();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app! 📨",
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    } else {
+      Alert.alert(
+        "Unable to schedule notification",
+        "Enable the notifications permission for Expo Go in settings",
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        onPress={scheduleNotification}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,9 +40,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
   },
-  text: {
-    fontSize: 24,
+  button: {
+    backgroundColor: "#333",
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
